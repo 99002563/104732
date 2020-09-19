fdf1cd0485786c461863d1ba029bf0f51b4bbe5b
99002563 <71481192+99002563@users.noreply.github.com>
2020-09-19 13:42:44 +0530

    Update calculator.h

--- include/calculator.h
+++ include/calculator.h
@@ -1,15 +1,12 @@
 #ifndef CALCULATOR_H_INCLUDED
 #define CALCULATOR_H_INCLUDED
-/*
+
 long int sum(long int,long int);
 long int sub(long int,long int);
 long int mul(long int,long int);
 long int div(long int,long int);
 long int mod(long int,long int);
 long int who(long int,long int);
-*/
-double evaluate (char []);
-int precedence(char);
-double calculate(char , double , double );
-char checknumber(char);
+
+
 #endif // CALCULATOR_H_INCLUDED

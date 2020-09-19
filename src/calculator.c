7f4670a6a6ccf7cd2e856af652abe4b05ba343bf
99002563 <71481192+99002563@users.noreply.github.com>
2020-09-19 11:52:06 +0530

    Create calculator.c

---
+++ src/calculator.c
@@ -0,0 +1,20 @@
+#include "calculator.h"
+
+int sum(int a,int b){
+return(a+b);
+}
+int sub(int a,int b){
+return(a-b);
+}
+int mul(int a,int b){
+return(a*b);
+}
+int div(int a,int b){
+return(a/b);
+}
+int mod(int a,int b){
+return(a%b);
+}
+int who(int a,int b){
+return((a*a)+(b*b)+(2*a*b));
+}

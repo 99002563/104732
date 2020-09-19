4b5f590b272a73236093e921a1988e60e89d675c
99002563 <71481192+99002563@users.noreply.github.com>
2020-09-19 11:52:56 +0530

    Create calculator.h

---
+++ include/calculator.h
@@ -0,0 +1,11 @@
+#ifndef CALCULATOR_H_INCLUDED
+#define CALCULATOR_H_INCLUDED
+
+int sum(int,int);
+int sub(int,int);
+int mul(int,int);
+int div(int,int);
+int mod(int,int);
+int who(int,int);
+
+#endif // CALCULATOR_H_INCLUDED

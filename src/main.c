a10b4a7d493e52af8ac584bf5c5000cb2bd2a61f
99002563 <71481192+99002563@users.noreply.github.com>
2020-09-19 13:44:11 +0530

    Update main.c

--- src/main.c
+++ src/main.c
@@ -3,7 +3,7 @@
 #include<math.h>
 #include<stdlib.h>
  int main(){
- /* long int a,b;
+ long int a,b;
  scanf("%lu%lu",&a,&b);
  printf("Sum is %lu",sum(a,b));
  printf("Sum is %lu",sub(a,b));
@@ -12,12 +12,6 @@
  printf("Sum is %lu",mod(a,b));
  printf("Sum is %lu",who(a,b));
  return 0;
- */
-    char expression[100];
-    double result;
-
-    printf("Enter the Expression: "); scanf("%[^\n]", expression);
-    result = evaluate(expression);
-    printf("Result = %lf\n", result);
-    return 0;
+ 
+    
  }
